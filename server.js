f24b7b30c1681f13d7bb6663186d01d714c42295
Leon Chang <godknows963852741@gmail.com>
2024-02-29 06:05:01 +0800

    dir modified

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, "frontend", "build")));
+app.use(express.static(path.join(__dirname, "./frontend/build")));
 //觀察req.body
 app.use((req, res, next) => {
   console.log("Request body:", req.body);
@@ -47,7 +47,7 @@ if (
   process.env.NODE_ENV === "staging"
 ) {
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
   });
 }
 app.listen(port, () => {
