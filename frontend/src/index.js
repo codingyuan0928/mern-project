77444d35d4f5d385626294c535cdf09ed2985080
Leon Chang <godknows963852741@gmail.com>
2024-02-29 23:17:54 +0800

    react -v18 syntax fixed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./styles/style.scss";
 import { PayPalScriptProvider } from "@paypal/react-paypal-js";
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 const initialOptions = {
   "client-id":
     "AUf0C7tKNMaLHS-OVLirRFAzDC2jgaX4GAn8p0tQhvplGf31eL1Tdh9a0mezJ92J4mo1ODmD9RqqVXB6",
